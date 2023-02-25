0d3f2acbfa6b22ccd26085f54e1a850b5fb90260
Atyson Jaime <atysonjaime@gmail.com>
2023-02-25 19:31:22 -0300

    :zap: Melhora o condicional do componente de ShoppingCart

--- components/Header/components/ShoppingCart/index.jsx
+++ components/Header/components/ShoppingCart/index.jsx
@@ -2,39 +2,23 @@ import { Avatar, AvatarBadge } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
 
 export default function ShoppingCart(props) {
-  let badge;
-  if (props.mostrarQuantidade) {
-    if (props.quantidadeNoCarrinho > 10) {
-      badge = (
-        <AvatarBadge
-          width="1.9rem"
-          height="1.5rem"
-          bg="green.500"
-          fontSize="0.75rem"
-        >
-          +10
-        </AvatarBadge>
-      );
-    } else {
-      badge = (
-        <AvatarBadge
-          width="1.9rem"
-          height="1.5rem"
-          bg="green.500"
-          fontSize="0.75rem"
-        >
-          {props.quantidadeNoCarrinho}
-        </AvatarBadge>
-      );
-    }
-  }
-
   return (
     <Avatar
       className="avatar-carrinho"
       icon={<FiShoppingCart fontSize="1.5rem" />}
     >
-      {badge}
+      {props.mostrarQuantidade ? (
+        <AvatarBadge
+          width="1.9rem"
+          height="1.5rem"
+          bg="green.500"
+          fontSize="0.75rem"
+        >
+          {props.quantidadeNoCarrinho > 10 ? "+10" : props.quantidadeNoCarrinho}
+        </AvatarBadge>
+      ) : (
+        false
+      )}
     </Avatar>
   );
 }
