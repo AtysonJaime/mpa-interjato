735e32ffb338fa693cf5738326c777ace295dff4
Atyson Jaime <atysonjaime@gmail.com>
2023-02-26 15:04:03 -0300

    :zap: Cria estado global para os produtos

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import catalogo from "../assets/json/catalogo.json";
 
 function MyApp({ Component, pageProps }) {
+  const [listProdutos, setListProdutos] = React.useState(catalogo.produtos);
   return (
     <>
       <Head>
         <title>MPA - Mercado Produtos Agricolas</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <Component
+        listProdutos={listProdutos}
+        setListProdutos={setListProdutos}
+        {...pageProps}
+      />
     </>
   );
 }
