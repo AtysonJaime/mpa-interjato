3a595f7d8e7ec3f1421a01289c46d2be91b1714d
Atyson Jaime <atysonjaime@gmail.com>
2023-02-26 15:28:47 -0300

    :art: :lipstick: Finaliza componente de Produtos para página inicial

--- components/Produtos/index.jsx
+++ components/Produtos/index.jsx
@@ -1,6 +1,8 @@
-import { Divider } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Link } from "@chakra-ui/react";
 import styled from "styled-components";
+import CardProduto from "../CardProduto";
 import TitleSection from "../TitleSection";
+import pallets from "../../assets/json/pallets.json";
 
 const ProdutosStyled = styled.div`
   display: flex;
@@ -14,14 +16,49 @@ const ProdutosStyled = styled.div`
   @media (max-width: 576px) {
     padding: 0 0.9375rem;
   }
+
+  .button-link {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 0.9375rem;
+
+    a {
+      text-decoration: none;
+    }
+    button {
+      background-color: ${pallets.wheat};
+      color: ${pallets["darker-wheat"]};
+      border: 1px solid ${pallets["dark-wheat"]};
+      &:hover,
+      &:focus {
+        transition: all 0.2s;
+        background-color: ${pallets["dark-wheat"]};
+        color: ${pallets.white};
+      }
+    }
+  }
 `;
-export default function Produtos() {
+export default function Produtos(props) {
   return (
     <ProdutosStyled>
       <TitleSection
         title="Produtos"
-        subtitle="Nossos melhores produtos para sua lavoura"
+        subtitle="Seleçao dos 3 produtos mais vendidos"
       />
+      <SimpleGrid minChildWidth="280px" spacing="40px" mb="4">
+        {props.listProdutos
+          .filter((produto) => produto.maisVendido)
+          .map((produto) => {
+            return <CardProduto key={produto.id} produto={produto} />;
+          })}
+      </SimpleGrid>
+      <Box className="button-link">
+        <Link href="/produtos">
+          <Button variant="outline">Visualizar demais produtos</Button>
+        </Link>
+      </Box>
     </ProdutosStyled>
   );
 }
