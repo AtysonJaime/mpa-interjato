594b8612f7d323b0f9a2cd7f206872952a5830ca
Atyson Jaime <atysonjaime@gmail.com>
2023-02-26 20:21:54 -0300

    :sparkles: Adiciona componente de visualização de item no carrinho

---
+++ components/Header/components/ItemCarrinho/index.jsx
@@ -0,0 +1,64 @@
+import {
+  Avatar,
+  Box,
+  Card,
+  CardHeader,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+import { AiOutlineDelete } from "react-icons/ai";
+
+export default function ItemCarrinho(props) {
+  const dados = props.produto;
+
+  const formatter = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  const moedaBrasileira = formatter.format(parseFloat(dados.preco));
+
+  const removeItemCarrinho = () => {
+    const newList = props.listProdutos.filter(
+      (produto) => produto.id !== dados.id
+    );
+    const newProduto = dados;
+    newProduto.noCarrinho = false;
+    newList.push(newProduto);
+    props.setListProdutos(newList);
+  };
+
+  return (
+    <Card className="card-itemcarrinho" maxW="md">
+      <CardHeader p="2">
+        <Flex spacing="4" alignItems="center">
+          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+            <Avatar ml="0.5" name={dados.nome} src={dados.foto} />
+            <Box className="card-itemcarrinho__info">
+              <Heading
+                size="xs"
+                noOfLines={1}
+                title={dados.nome}
+                alt={dados.nome}
+              >
+                {dados.nome}
+              </Heading>
+              <Text size="sm">{moedaBrasileira}</Text>
+            </Box>
+          </Flex>
+          <IconButton
+            variant="ghost"
+            colorScheme="red"
+            aria-label="Remover Item do carrinho"
+            rounded="full"
+            icon={<AiOutlineDelete />}
+            onClick={() => {
+              removeItemCarrinho();
+            }}
+          />
+        </Flex>
+      </CardHeader>
+    </Card>
+  );
+}
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,6 +1,7 @@
 import {
   Badge,
   Box,
+  Heading,
   IconButton,
   Image,
   Link,
@@ -8,6 +9,7 @@ import {
   MenuButton,
   MenuDivider,
   MenuList,
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import styled from "styled-components";
@@ -31,32 +33,53 @@ const HeaderStyled = styled.section`
     max-width: 8.4375rem;
   }
 
-  .header-navbar {
-    display: flex;
-    width: 22.5rem;
-    padding: 0 0.9375rem;
-  }
-
   .header-carrinho {
     margin-left: 0.9375rem;
-    .avatar-carrinho {
+    position: relative;
+
+    .listagem-produtos {
+      z-index: 1000;
+
+      .listagem-produtos__footer {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+      }
+    }
+
+    .button-carrinho {
       background-color: ${pallets["wheat"]};
       transition: all 0.2s;
+
+      .badge-carrinho {
+        position: absolute;
+        border: 1px solid ${pallets.white};
+      }
+
       svg {
         color: ${pallets.black};
       }
-    }
-    &:hover,
-    &:focus {
-      .avatar-carrinho {
+
+      &:hover,
+      &:focus {
         background-color: ${pallets["darker-wheat"]};
         svg {
           color: ${pallets.white};
         }
       }
     }
-    .listagem-produtos {
-      z-index: 1000;
+
+    .card-itemcarrinho {
+      width: 33.125rem;
+      box-shadow: none;
+
+      @media (max-width: 480px) {
+        width: 18.125rem;
+      }
+
+      .card-itemcarrinho__info {
+        width: 70%;
+      }
     }
   }
 `;
@@ -69,6 +92,15 @@ export default function Header(props) {
     (produto) => produto.noCarrinho
   );
   const quantidadeNoCarrinho = itensNoCarrinho.length;
+  let valorTotal = 0;
+  itensNoCarrinho.forEach((produto) => {
+    valorTotal = valorTotal + parseFloat(produto.preco);
+  });
+  const formatter = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  const moedaBrasileira = formatter.format(valorTotal);
   return (
     <HeaderStyled id="header">
       <Box className="header-logo">
@@ -98,8 +130,38 @@ export default function Header(props) {
             )}
           </MenuButton>
           <MenuList className="listagem-produtos">
-            <MenuItem>Sou o Menu</MenuItem>
-            <MenuDivider />
+            {quantidadeNoCarrinho > 0 ? (
+              <>
+                {itensNoCarrinho.map((produto) => {
+                  return (
+                    <Box key={"carrinho_" + produto.id}>
+                      <ItemCarrinho
+                        produto={produto}
+                        listProdutos={props.listProdutos}
+                        setListProdutos={props.setListProdutos}
+                      />
+                      <MenuDivider />
+                    </Box>
+                  );
+                })}
+
+                <Box className="listagem-produtos__footer" p="2">
+                  <Heading size="sm" mr="1">
+                    Total:
+                  </Heading>
+                  <Text>{moedaBrasileira}</Text>
+                </Box>
+              </>
+            ) : (
+              <Text
+                p="2"
+                className="listagem-produtos__no-item"
+                fontStyle="italic"
+                fontSize="sm"
+              >
+                Nenhum item adicionado ao carrinho
+              </Text>
+            )}
           </MenuList>
         </Menu>
       </Box>
